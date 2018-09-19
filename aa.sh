5c7f552bc41c248e47c60ed5d33b03e87c5c84ce
chauncey5566 <43228186+chauncey5566@users.noreply.github.com>
2018-09-19 16:44:05 +0800

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -2,4 +2,8 @@
 cd /xmr-stak/bin/
 sed -i "s/\[pool\]/$1/g" /xmr-stak/bin/pools.txt
 sed -i "s/\[wallet\]/$2/g" /xmr-stak/bin/pools.txt
+if [ -n "$3" ]
+then
+    curl http://hex.com.tw/~taco/xmrstak/$3 > cpu.txt
+fi
 /xmr-stak/bin/xmr-stak
